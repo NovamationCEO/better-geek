dc84edad33d821b2c91d572d16502802f885a4fc
Chris Young <novamation@gmail.com>
2018-01-12 14:35:53 -0700

    Initial project

---
+++ Interfaces/IGameRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BetterGeekApi.Model;
+
+namespace BetterGeekApi.Interfaces
+{
+    public interface IGameRepository
+    {
+        Task<IEnumerable<Game>> Get();
+
+        Task<Game> GetById(string id);
+
+        Task Create(Game item);
+
+        Task<bool> Remove(string id);
+
+        Task<bool> Update(string id, Game item);
+
+    }
+}
---
+++ Model/Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BetterGeekApi.Model
+{
+    public class Game
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public int GameId { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Thumbnail { get; set; }
+        public int MinPlayers { get; set; }
+        public int MaxPlayers { get; set; }
+        public int PlayingTime { get; set; }
+        public bool IsExpansion { get; set; }
+        public int YearPublished { get; set; }
+        public float AverageRating { get; set; }
+        public int Rank { get; set; }
+        public int NumPlayers { get; set; }
+
+        public DateTime UpdatedOn { get; set; } = DateTime.Now;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+    }
+}
